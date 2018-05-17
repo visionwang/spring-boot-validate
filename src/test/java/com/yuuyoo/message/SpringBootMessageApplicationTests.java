b7df01b37b0306c504ef149f7944b6c41eb633bb
dave <dave@yuuyoo.com>
2018-05-17 08:09:10 +0800

    pro init

---
+++ src/test/java/com/yuuyoo/message/SpringBootMessageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yuuyoo.message;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMessageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
